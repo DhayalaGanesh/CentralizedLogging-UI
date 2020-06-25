ed5d1f248802660d30f761f368c5b3c1f310344f
DhayalaGanesh <dhayal1997@gmail.com>
2020-06-25 15:31:56 +0530

    Config changes

--- src/app/Services/getserviceslist.services.ts
+++ src/app/Services/getserviceslist.services.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class ServicesList{
     constructor(private http: HttpClient){}
     public GetServicesList(){
-        return this.http.get('http://localhost:60335/Services/GetServicesList');
+        return this.http.get(environment.apiUrl + 'Services/GetServicesList');
     }
 }
--- src/app/Services/servicelogs.services.ts
+++ src/app/Services/servicelogs.services.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class ServicesLogs{
     constructor(private http: HttpClient){}
     public GetServiceLogs(serviceName: string){
-        return this.http.get('http://localhost:60335/Services/GetServicesLogs?serviceName='+serviceName).toPromise();
+        return this.http.get(environment.apiUrl + 'Services/GetServicesLogs?serviceName='+serviceName).toPromise();
     }
 }
